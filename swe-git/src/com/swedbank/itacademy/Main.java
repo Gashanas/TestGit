40221a6522c54e14efce3f7083409a3430b8a234
Gashanas <gashanas@gmail.com>
2017-02-12 18:52:31 +0200

    Return 9

---
+++ swe-git/src/com/swedbank/itacademy/Main.java
@@ -0,0 +1,9 @@
+package com.swedbank.itacademy;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello IT academy!!1");
+        return 9;
+    }
+}
